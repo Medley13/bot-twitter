a0073ebe58d345d27ab500975a5f30fc1a18a7fb
catheo <theo.cappadona@ynov.com>
2024-11-17 21:19:57 +0100

    Ajout du code Python et du dossier bot-twitter

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
+import tweepy
 import os
 from dotenv import load_dotenv
 
-# Charger les variables depuis le fichier .env
+# Charger les variables d'environnement
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+API_KEY = os.getenv("AG5RhI0Cunza7BbZFJaydIsrWK")
+API_SECRET = os.getenv("Um3CDHeNki64oeeaUNP59ECSxYnWZFR6m9gzP8t9u2cqKyPrEy")
+ACCESS_TOKEN = os.getenv("1823714527575465991-cKpgWFQHc5zZ2oQnA2devzDqnaM6l6")
+ACCESS_TOKEN_SECRET = os.getenv("4foEG2ycBOmDp8eRmOOcwMzfusytuW54v1LurzhPE50sf")
 
-print(f"Clé API : {API_KEY}")  # Vérifiez si les clés sont chargées
+# Authentification avec Twitter API
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Créer une instance de l'API Tweepy
+api = tweepy.API(auth)
+
+# Exemple d'envoi d'un tweet
+api.update_status("Hello, Twitter! Ce tweet est envoyé par un bot.")
